86b6f9116f887f6471b3b58bdfeb72c2445e4d45
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-03-03 14:37:18 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
