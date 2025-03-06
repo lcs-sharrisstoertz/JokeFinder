48a1445fffa32cc7fb82c0fa6d762cac0eb931ba
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-03-06 10:55:47 -0500

    Made it possible to share a saved joke.

--- JokeFinder/View/FavouriteJokesView.swift
+++ JokeFinder/View/FavouriteJokesView.swift
@@ -54,6 +54,16 @@ struct FavouriteJokesView: View {
                                     }
                                 }
                                 
+                                // Share
+                                ShareLink(
+                                    "Share",
+                                    item: currentJoke.setupAndPunchline,
+                                    preview: SharePreview(
+                                        "Share Joke",
+                                        image: Image("ShareJokeImage")
+                                    )
+                                )
+                                
                             }
                             
                         }
